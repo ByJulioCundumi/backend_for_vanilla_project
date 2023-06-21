7662fbdce81d0ac6fa126e09ae71b5b230fd240d
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-06-20 23:12:00 -0500

    rest api completed and working :), ready to deploy

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -5,40 +5,84 @@ export const getUsers = async (req, res) => {
         const [rows] = await pool.query("SELECT * FROM users")
         res.json(rows)
     } catch (error) {
+        console.log(error)
         res.status(500).json({
-            message: "Something wrong happened :("
+            message: "Something wrong happened, users not found :("
         })
     }
 }
 //
 export const getUser = async (req, res) => {
+    const {id} = req.params;
     try {
-        
+        const [rows] = await pool.query("SELECT * FROM users WHERE id=?", [id])
+        if(rows.length <= 0){
+            res.status(404).json({
+                message: "User not found :("
+            })
+        }
+        res.json(rows[0])
     } catch (error) {
-        
+        console.log(error)
+        res.status(500).json({
+            message: "Something wrong happened, user not found :("
+        })
     }
 }
 //
-export const postUser = (req, res) => {
+export const postUser = async (req, res) => {
+    const {firstname, lastname, email, pass} = req.body;
     try {
-        
+        const [rows] = await pool.query("INSERT INTO users (firstname, lastname, email, pass) VALUES (?,?,?,?)", [firstname, lastname, email, pass]);
+        res.send({
+            id: rows.insertId,
+            firstname,
+            lastname,
+            email,
+            pass
+        })
     } catch (error) {
-        
+        console.log(error)
+        res.status(500).json({
+            message: "Something wrong happened, user not posted :("
+        })
     }
 }
 //
 export const putUser = async (req, res) => {
+    const {id} = req.params;
+    const {firstname, lastname, email, pass} = req.body;
     try {
-        
+        const [result] = await pool.query("UPDATE users SET firstname=?, lastname=?, email=?, pass=? WHERE id=?", [firstname, lastname, email, pass, id])
+        if(result.affectedRows <= 0){
+            res.status(404).json({
+                message: "User not found :("
+            })
+        }
+        const [rows] = await pool.query("SELECT * FROM users WHERE id=?", [id])
+        res.json(rows[0])
     } catch (error) {
-        
+        console.log(error)
+        res.status(500).json({
+            message: "Something wrong happened, user not updated :("
+        })
     }
 }
 //
-export const deleteUser = (req, res) => {
+export const deleteUser = async (req, res) => {
+    const {id} = req.params;
     try {
-        
+        const [result] = await pool.query("DELETE FROM users WHERE id=?", [id])
+        if(result.affectedRows <= 0){
+            res.status(404).json({
+                message: "User not found"
+            })
+        }
+        res.sendStatus(204)
     } catch (error) {
-        
+        console.log(error)
+        res.status(500).json({
+            message: "Something wrong happened, user not deleted :("
+        })
     }
 }
