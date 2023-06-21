bd9f9ce317927213ffc2258ecc21b7c5bf926ed0
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-06-20 21:20:55 -0500

    endpoints added and configuration of the controllers in progress

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import Express from "express";
+import { router as usersRoutes } from "./routes/users.routes.js";
+//
 const app = Express()
-
+//
+app.use(Express.json())
+app.use("/api", usersRoutes)
+//
 export default app;
---
+++ src/controllers/users.controller.js
@@ -0,0 +1,41 @@
+import {pool} from "../db.js"
+
+export async function getUsers(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+//
+export async function getUser(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+//
+export async function postUser(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+//
+export async function putUser(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+//
+export async function deleteUser(req, res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
