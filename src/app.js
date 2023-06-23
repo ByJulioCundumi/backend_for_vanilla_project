d93ead928a7ecda6f173db929f0540d56cc15bad
ByJulioCundumi <julio.ce.dev@gmail.com>
2023-06-23 13:02:03 -0500

    cors added for all the endpoints

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { router as usersRoutes } from "./routes/users.routes.js";
 //
 const app = express()
 //
 app.use(express.json())
+app.use(cors())
 app.use("/api", usersRoutes)
 //
 app.use((req, res)=>{
